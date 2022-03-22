dd6d4eff9cc5b22bcd81241041f064d7adc3b7ad
Alejandro0702 <rodolfo.franco18@tectijuana.edu.mx>
2022-03-22 11:29:24 -0700

    Test Respuesta del teclado

--- script.js
+++ script.js
@@ -1,58 +1,66 @@
 let accionesArray = {
-    1:"ArrowUp",
-    2:"ArrowLeft",
-    3:"ArrowDown",
-    4:"ArrowRight"
+    1:'KeyW',
+    2:'KeyA',
+    3:'KeyS',
+    4:'KeyD'
 };
 let temporal = [];
-let c, a;
+let a, c;
+let inicio, limMin, limMax, incr;
 
 function Comenzar(){
-    c = 0;
     a = 0;
+    c = 0;
+    incr = 2;
+    limMin = incr;
+    limMax = 6;
     SimonDice();
 }
 function SimonDice(){
-    document.removeEventListener('keydown', this, false);
-    for (let i = 0; i < 5; i++) {
+    inicio = 0;
+    console.log("Lim min: " + limMin);
+    for (let i = 0; (i < incr); i++) {
         let r = Random(1, 5);
         temporal.push(accionesArray[r]);
     }
+    c = 0;
     Acciones();
 }
 function Acciones(){
     console.log(temporal[c] + "");
     c++;
-    if(c<4){
+    if(c<temporal.length){
         setTimeout(Acciones, 1000);
     }
     else{
-        Respuesta();
+        document.addEventListener('keypress', Respuesta);
+    }
+}
+
+function Respuesta(e){
+    tecla = e.code;
+    if(tecla == temporal[inicio]){
+        alert("SI \n" + " tecla presionada: " + tecla + "\n\nSimon dijo esto: " + temporal[inicio]);
+        inicio++;
+
+    }else{
+        alert("NO \n" + " tecla presionada: " + tecla + "\n\nSimon Dijo esto xd: " + temporal[inicio]);
+        inicio++;
+    }
+    if(inicio == limMin){
+        if(limMin < limMax){
+            alert("SIGUIENTE FASE");
+            console.clear();
+            limMin+=2;
+            document.removeEventListener('keypress', Respuesta);
+            SimonDice();
+        }
+        else{
+            alert("JUEGO TERMINADO");
+            document.location.reload(true);
+        }
     }
 }
 function Random(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
-}
-
-function Respuesta(){
-    document.addEventListener('keydown', (event) => {
-        a++;
-        const tecla = event.key;
-        switch (tecla) {
-            case 'ArrowUp':
-                alert('keydown event\n\n' + 'key: ' + tecla + a);
-                break;
-            case 'ArrowRight':
-                alert('keydown event\n\n' + 'key: ' + tecla);
-                break;
-            case 'ArrowDown':
-                alert('keydown event\n\n' + 'key: ' + tecla);
-                break;
-            case 'ArrowLeft':
-                alert('keydown event\n\n' + 'key: ' + tecla);
-                break;       
-            default:
-                break;
-        }
-    });
 }
